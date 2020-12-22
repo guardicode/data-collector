5bedf14bb72a8becd41875a0e17131afed8a9af4
naamasender <74241986+naamasender@users.noreply.github.com>
2020-12-22 18:42:19 +0200

    Code update from Omer
    Fix error: Error copying collect_data_from_windows_machines.bat to remote system: The file exists.

--- PsExec/windows_data_collector.bat
+++ PsExec/windows_data_collector.bat
@@ -1,19 +1,17 @@
 @echo off
 setlocal EnableDelayedExpansion
-
-	if not exist results (mkdir results)
+    if not exist results (mkdir results)
     set /p defaultuser="Enter an Administrator username: "
     set /p defaultpass="Enter password: "
-	for /F "tokens=1-3 delims=," %%i in (windows_machines.ini) do (
-	   set ip=%%i
-	   if %%j==default (set username=!defaultuser!) else (set username=%%j)
-	   if %%k==default (set passwd=!defaultpass!) else (set passwd=%%k)
-       PSexec.exe \\!ip! -u !username! -p !passwd! -c collect_data_from_windows_machines.bat > results\output-!ip!.txt
-	)
-	if exist "C:\Program Files\7-Zip\7z.exe" (
-		"C:\Program Files\7-Zip\7z.exe" a -tzip results.zip -r results
-	) else (
-		echo 7zip not in default path C:\Program Files\7-Zip\7z.exe
-	)
-
+    for /F "tokens=1-3 delims=," %%i in (windows_machines.ini) do (
+       set ip=%%i
+       if %%j==default (set username=!defaultuser!) else (set username=%%j)
+       if %%k==default (set passwd=!defaultpass!) else (set passwd=%%k)
+       PSexec.exe \\!ip! -u !username! -p !passwd! -c -f collect_data_from_windows_machines.bat > results\output-!ip!.txt
+    )
+    if exist "C:\Program Files\7-Zip\7z.exe" (
+        "C:\Program Files\7-Zip\7z.exe" a -tzip results.zip -r results
+    ) else (
+        echo 7zip not in default path C:\Program Files\7-Zip\7z.exe
+    )
 endlocal
